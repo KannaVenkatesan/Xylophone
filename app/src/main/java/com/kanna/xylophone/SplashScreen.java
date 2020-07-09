235b85853930a5397d5536f0498a7ffd5dc2da8d
kanna <kannavenkatesan466@gmail.com>
2020-07-09 16:34:24 +0530

    Xylophone app

---
+++ app/src/main/java/com/kanna/xylophone/SplashScreen.java
@@ -0,0 +1,21 @@
+package com.kanna.xylophone;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
